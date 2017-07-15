f168187a197cfb364e8d90d05a7e6ec3ae4a7918
JesusSalasMataro <jesus459@hotmail.es>
2017-07-15 19:55:10 +0200

    Comics mapper test (red)

---
+++ js/tests/comics_mapper_unit_tests.js
@@ -0,0 +1,41 @@
+describe("Given an array of Marvel comics convert them to Stuart comics", function() {
+ 
+    let _sut;
+
+    beforeEach(function() {
+         _sut = new ComicsMapper();
+    });
+
+    it("when receive Marvel comics return Stuart Comics", function() {
+        // ARRANGE
+        marvelComics = 
+            { 
+                "comics": [
+                    {
+                        id: 1, 
+                        title: "Hulk", 
+                        thumbnail: {
+                            path: "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available", 
+                            extension: ""
+                        }, 
+                        prices: [
+                            {
+                                type: "printPrice",
+                                price: 20
+                            }
+                        ]
+                    }
+                ]
+            };
+
+        // ACT        
+        let stuartComics = _sut.toStuartComics(marvelComics);
+
+        // ASSERT
+        expect(stuartComics.length).toBeGreaterThan(0);
+        expect(stuartComics[0].title).toBe('Hulk');
+        expect(stuartComics[0].thumbnailUrl).toBe('http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg');
+        expect(stuartComics[0].price).toBe(20);
+    });                                   
+ 
+});
